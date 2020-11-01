c1183da3c33d4deb0d35b51fef05b18ffe74d90a
unknown <saroamirkhanyan@gmail.com>
2020-11-01 15:10:14 +0400

    configured router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import express from 'express'
 import mongoose from "mongoose"
+import routes from './routes'
+
+import cors from "cors"
 import "dotenv/config"
 // Initializing App
 const app = express()
 
+// Connect to Database
 mongoose.connect(process.env.DATABASE_CONNECT,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
   },
-  err => console.log("Connected to db!"))
+  err => console.log(err)
+)
 
-
+//Middlware
+app.use(cors())
+app.use('/', routes)
