1df6838588e44dd19d39076409878dad39c73b29
Saro Amirkhanyan <saroamirkhanyan@gmail.com>
2020-12-01 15:37:17 +0400

    added # before hashtags and space between hashtags

--- src/components/Content/Main/Jobs/JobCard.jsx
+++ src/components/Content/Main/Jobs/JobCard.jsx
@@ -1,41 +1,44 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Button, Wrapper } from '../../../../styled/StyledElements'
-import CardDescription from './CardDescription'
+import React from "react";
+import styled from "styled-components";
+import { Button, Wrapper } from "../../../../styled/StyledElements";
+import CardDescription from "./CardDescription";
 
 const JobCardStyled = styled.section`
-  background-color: ${(props) => props.theme.primaryBg};
-  color: ${(props) => props.theme.primaryColor};
-  border-radius: 20px;
-  padding: 15px;
-  display: flex;
-  flex: 1 0 auto;
-  flex-direction: column;
-  row-gap: 10px;
-`
+    background-color: ${(props) => props.theme.primaryBg};
+    color: ${(props) => props.theme.primaryColor};
+    border-radius: 20px;
+    padding: 15px;
+    display: flex;
+    flex: 1 0 auto;
+    flex-direction: column;
+    row-gap: 10px;
+`;
 
 const CardButton = styled(Button)`
-  padding: 5px 10px;
-`
+    padding: 5px 10px;
+`;
 
 const CardWrapper = styled(Wrapper)`
-  row-gap: 15px;
-`
+    row-gap: 15px;
+`;
 
 const CardHashtegs = styled.p`
-  font-size: 15px;
-`
+    font-size: 15px;
+`;
 
 function JobCard({ hashtags, price, description }) {
-  return (
-    <JobCardStyled>
-      <CardDescription>{description}</CardDescription>
-      <CardWrapper justify="space-between" align="center">
-        <CardHashtegs>{hashtags}</CardHashtegs>
-        <CardButton>{price}</CardButton>
-      </CardWrapper>
-    </JobCardStyled>
-  )
+    console.log(hashtags);
+    return (
+        <JobCardStyled>
+            <CardDescription>{description}</CardDescription>
+            <CardWrapper justify="space-between" align="center">
+                <CardHashtegs>
+                    {hashtags.map((hashtag) => "#" + hashtag).join(" ")}
+                </CardHashtegs>
+                <CardButton>{price}</CardButton>
+            </CardWrapper>
+        </JobCardStyled>
+    );
 }
 
-export default JobCard
+export default JobCard;
