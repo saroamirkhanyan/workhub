38513958b42fa2f48c1737244cd4ce43c92ae00a
VaruZHV <varuzhan.g1.y@tumo.com>
2020-12-24 19:32:32 +0300

    commit

--- src/components/SignComponent/SignIn.jsx
+++ src/components/SignComponent/SignIn.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function SignIn() {
-  return <div></div>
+  return <h1>Signin</h1>
 }
 
 export default SignIn
--- src/components/SignComponent/SignUp.jsx
+++ src/components/SignComponent/SignUp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function SignUp() {
-  return <div></div>
+  return <h1>signUp</h1>
 }
 
 export default SignUp
